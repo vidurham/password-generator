b7c0db2dfddf4392bf6d926f75da593aacc26e81
Vinny D <vdurham@prideseals.com>
2022-02-09 18:44:34 -0500

    almost debugged

--- Develop/script.js
+++ Develop/script.js
@@ -10,41 +10,42 @@ var password = ""
     passwordLength = parseInt(passwordLength)
     console.log(passwordLength)
     if (isNaN(passwordLength)) {
-      window.alert("Invalid response! Please enter a number between 8 - 128")
-      generatePassword()
+      window.alert("Invalid response! Please enter a number between 8 - 128");
+      writePassword();
     }
     else if (passwordLength < 8 || passwordLength > 128) {
-      window.alert("Invalid number of characters! Please enter a number between 8 - 128")
-      generatePassword()
+      window.alert("Invalid number of characters! Please enter a number between 8 - 128");
+      writePassword();
     }
     else {
       var lowerCaseConfirm = window.confirm("Would you like there to be lowercase characters in your password?")
         if (lowerCaseConfirm) {
-          characters += lowercase
-          console.log(characters)
+          characters += lowercase;
+          console.log(characters);
         }
       var upperCaseConfirm = window.confirm("Would you like there to be uppercase characters in your password?")
         if (upperCaseConfirm) {
-          characters += uppercase
-          console.log(characters)
+          characters += uppercase;
+          console.log(characters);
         }
       var numberConfirm = window.confirm("Would you like there to be numbers in your password?")
         if (numberConfirm) {
-          characters += numbers
-          console.log(characters)
+          characters += numbers;
+          console.log(characters);
         }
       var specialConfirm = window.confirm("Would you like there to be special characters in your password?")
         if (specialConfirm) {
-          characters += special
-          console.log(characters)
+          characters += special;
+          console.log(characters);
         }
         for (var i = 0; i < passwordLength; i++) {
           password += characters.charAt(Math.floor(Math.random()*characters.length))
         }
-        console.log(password)
+        console.log(password);
         return password;
-  }
+    }
 }
+
  
 
 
@@ -58,7 +59,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
