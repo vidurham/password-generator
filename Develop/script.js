c615064abf49b33d28b9e21cbf29787e173c2bc4
Vinny D <vdurham@prideseals.com>
2022-02-09 13:18:04 -0500

    got password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,25 @@
-// Assignment code here
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var passwordLength = ""
+var getPasswordLength = function () {
+  var passwordLength = window.prompt("How many characters would you like your password to be? (8-128 characters)");
+  passwordLength = parseInt(passwordLength);
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Invalid number of characters! Please enter a number between 8 - 128")
+    getPasswordLength()
+  }
+  else {
+  }
+}
+getPasswordLength()  
+
+console.log(passwordLength)
+
+var upperCaseConfirm = window.confirm("Would you like there to be uppercase characters in your password?")
+
+var numberConfirm = window.confirm("Would you like there to be numbers in your password?")
+
+var specialConfirm = window.confirm("Would you like there to be special characters in your password?")
+
 
 
 // Get references to the #generate element
